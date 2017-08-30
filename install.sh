7a2adda78ca56a874e85efcd5eea0c37853f6a1c
Gourin Maison <gourin.maison@google.com>
2017-08-30 20:03:55 +0100

    Use Jenkins jobs to trigger deployments

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-rm -f $HOME/test.sh
-echo "#!/bin/sh" >> $HOME/test.sh
-echo "echo 'Content-type: text/html\n'" >> $HOME/test.sh
-echo "echo '<html><body>Hello world</body></html>'" >> $HOME/test.sh
+sudo docker pull jenkins/jenkins:lts-alpine
+sudo docker run -p 8080:8080 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
 
-sudo apt-get install apache2
-chmod 755 $HOME/test.sh
-sudo mv $HOME/test.sh /usr/lib/cgi-bin/
-sudo apachectl restart
-
-curl http://localhost/cgi-bin/test.sh
